a66e015cd3bafce13cf2a4028b0870095742cbb3
georgezikos <george.zikos@icloud.com>
2020-04-28 10:55:33 -0400

    Updates

---
+++ precache-manifest.f0d5d3709fbf11e46207770e0fd6045d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3776b4f9389abf1f043dcec0fe74f2a",
+    "url": "/learning2deploy/index.html"
+  },
+  {
+    "revision": "e31ee6fada8b89aafcc6",
+    "url": "/learning2deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "96cb49c4b14de86f3723",
+    "url": "/learning2deploy/static/js/2.2bf54af1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning2deploy/static/js/2.2bf54af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31ee6fada8b89aafcc6",
+    "url": "/learning2deploy/static/js/main.a0f743a3.chunk.js"
+  },
+  {
+    "revision": "f7a4084f8ef5e2078b9e",
+    "url": "/learning2deploy/static/js/runtime-main.2715b374.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learning2deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
